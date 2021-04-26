189fc3b4471cf4c6c637d1453d15d7209b91d23c
Jo3yVX <anhquyethgvx@gmail.com>
2021-04-26 08:35:17 +0700

    add fading effect

--- 2dOsu/include/Tile.h
+++ 2dOsu/include/Tile.h
@@ -8,6 +8,8 @@ const int mapRange = 4;
 const int EDGE = 160;
 const int OFFSET_BOTTOM = 40;
 
+int fade(void* data);
+
 class Tile {
 public:
 	Tile();
@@ -16,10 +18,13 @@ public:
 	void update();
 	void setBlackKey(bool isClick = false);
 	bool check(const int& mouseX, const int& mouseY);
+	int getLastRow() { return lastRow; }
+	int getLastCol() { return lastColumn; }
+
 private:
 	int map[mapRange][mapRange];
 	SDL_Texture* whiteTile;
-	SDL_Texture* blackTile;
+	//SDL_Texture* blackTile;
 	SDL_Rect src, dst;
 	int lastRow, lastColumn;
 	int score;
--- 2dOsu/src/Tile.cpp
+++ 2dOsu/src/Tile.cpp
@@ -5,8 +5,10 @@
 #include "../include/Timer.h"
 #include <string>
 #include <fstream>
+#include <SDL_thread.h>
 
 extern Music* music;
+
 Button* pause = NULL;
 TTF_Font* font30 = NULL;
 SDL_Color fontColor = { 0,0,0 };
@@ -16,10 +18,14 @@ Textbox* hiScorePoint = NULL;
 Textbox* timeText = NULL;
 Textbox* point = NULL;
 SDL_Color pointColor = { 28,135,179 };
+SDL_Color fadeColor = { 50, 168, 82 };
+SDL_Color tempFade = { 50, 168, 82 };
 Timer* countdown = NULL;
 Textbox* countdownText = NULL;
 
 TTF_Font* fontList[10]; //score effect  from size 30 -> 50, distance = 2
+
+
 
 Tile::Tile() {
 	//empty the map
@@ -30,7 +36,7 @@ Tile::Tile() {
 	}
 	for (int i = 0; i < mapRange - 1; i++) setBlackKey();
 	whiteTile = Texture::loadTexture("assets/whiteTile.png");
-	blackTile = Texture::loadTexture("assets/tile.png");
+	//blackTile = Texture::loadTexture("assets/tile.png");
 
 	src.x = src.y = 0;
 	src.w = dst.w = EDGE;
@@ -71,6 +77,8 @@ Tile::Tile() {
 	countdown = new Timer(20);
 	countdownText = new Textbox(to_string(countdown->getTimeLeft()), fontList[9], pointColor, 0, 50);
 	countdownText->center(ViewportX + EDGE / 2 + (mapRange - 1) * EDGE);
+
+	//init thread
 }
 
 Tile::~Tile() {
@@ -84,16 +92,34 @@ void Tile::render() {
 		for (int j = 0; j < mapRange; j++) {
 			type = map[i][j];
 
-			dst.x =  ViewportX + j * 160; //column
+			dst.x =  ViewportX + j * EDGE; //column
 			dst.y =  HEIGHT - mapRange * EDGE + ViewportY + i * 160 - OFFSET_BOTTOM; //row
 
 			switch (type) {
 			case 0:
-				//SDL_SetTextureColorMod(whiteTile, 173, 197, 183);
+				SDL_SetTextureColorMod(whiteTile, 255, 255, 255);
 				Texture::Draw(whiteTile, src, dst);
 				break;
 			case 1:
-				Texture::Draw(blackTile, src, dst);
+				SDL_SetTextureColorMod(whiteTile, 100, 100, 100);
+				Texture::Draw(whiteTile, src, dst);
+				//Texture::Draw(blackTile, src, dst);
+				break;
+			case 2:  //fucked up with fading effect
+				tempFade.r+=3;
+				tempFade.g+=3;
+				tempFade.b+=3;
+				if (tempFade.r > 255) tempFade.r = 255;
+				if (tempFade.g > 255) tempFade.g = 255;
+				if (tempFade.b > 255) tempFade.b = 255;
+				SDL_SetTextureColorMod(whiteTile, tempFade.r, tempFade.g, tempFade.b);
+				Texture::Draw(whiteTile, src, dst);
+				cout << (int)tempFade.r << endl;
+				//cout << tempFade.r << " " << tempFade.g << " " << tempFade.b << endl;
+				if (tempFade.r >= 254 || tempFade.g >= 254 || tempFade.b >= 254) {
+					map[i][j] = 0;
+					tempFade = fadeColor;
+				}
 				break;
 			default:
 				break;
@@ -109,7 +135,7 @@ void Tile::render() {
 	point->render();
 	hiScorePoint->render();
 	countdownText->render();
-	cout << 2 << endl;
+	
 }
 
 void Tile::update() {
@@ -122,7 +148,7 @@ void Tile::update() {
 		countdownText->update(to_string(countdown->getTimeLeft()), fontList[9], pointColor);
 		countdownText->center(ViewportX + EDGE / 2 + (mapRange - 1) * EDGE);
 	}
-	cout << 1 << endl;
+
 }
 
 void Tile::setBlackKey(bool isClick) {
@@ -131,13 +157,13 @@ void Tile::setBlackKey(bool isClick) {
 		do {
 			rRow = rand() % mapRange;
 			rCol = rand() % mapRange;
-		} while (map[rRow][rCol] == 1 || (lastRow == rRow && lastColumn == rCol));
+		} while (map[rRow][rCol] == 1 || map[rRow][rCol] == 2 || (lastRow == rRow && lastColumn == rCol));
 	}
 	else {
 		do {
 			rRow = rand() % mapRange;
 			rCol = rand() % mapRange;
-		} while (map[rRow][rCol] == 1);
+		} while (map[rRow][rCol] == 1 || map[rRow][rCol] == 2);
 	}
 	map[rRow][rCol] = 1;  // clickable, change color to black
 	cout << "(" << rRow << ":" << rCol << ")" << "       " << "(" << lastRow << ":" << lastColumn << ")" << endl;
@@ -156,7 +182,7 @@ bool Tile::check(const int& mouseX, const int& mouseY) {
 		if (map[row][column] == 1) {
 			int note = rand() % 16;
 			Mix_PlayChannel(-1, music->chunkList[note], 0);
-			map[row][column] = 0;
+			map[row][column] = 2;
 			score++;
 			point->update(to_string(score), fontList[9], pointColor);
 			lastRow = row;
@@ -179,3 +205,6 @@ bool Tile::check(const int& mouseX, const int& mouseY) {
 }
 	
 
+void fadeOut(int row, int column) {
+
+}
